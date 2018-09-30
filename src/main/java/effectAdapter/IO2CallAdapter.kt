0a17f92c2e434c79ca78d37e171e75997e5c91e0
Leandro Borges Ferreira <lehen01@gmail.com>
2018-09-30 17:45:31 -0300

    making non blocking calls

--- src/main/java/effectAdapter/IO2CallAdapter.kt
+++ src/main/java/effectAdapter/IO2CallAdapter.kt
@@ -1,15 +1,27 @@
 package effectAdapter
 
+import arrow.core.left
+import arrow.core.right
 import arrow.effects.IO
-import kotlinx.coroutines.experimental.CommonPool
 import retrofit2.Call
 import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.Response
 import java.lang.reflect.Type
 
 class IO2CallAdapter<R>(private val type: Type) : CallAdapter<R, Any> {
     override fun adapt(call: Call<R>): Any =
-        IO(CommonPool) {
-            call.execute().body()!!
+        IO.async<R> { callBack ->
+            call.enqueue(object: Callback<R> {
+                override fun onResponse(call: Call<R>, response: Response<R>) {
+                    //Todo: Deal with this '!!'
+                    callBack(response.body()!!.right())
+                }
+
+                override fun onFailure(call: Call<R>, throwable: Throwable) {
+                    callBack(throwable.left())
+                }
+            })
         }
 
     override fun responseType(): Type = type
--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,19 +1,16 @@
 import arrow.effects.IO
 import arrow.effects.effect
 import arrow.effects.fix
-import kotlinx.coroutines.experimental.CommonPool
 import retrofit.apiClient
 
 fun main(args: Array<String>) {
     apiClient()
         .getRepositoriesEffect("Java", "star", 1)
-        .executeCall(IO.effect(), CommonPool)
+        .executeCall(IO.effect())
         .fix()
         .unsafeRunSync()
         .items
-        .forEach { repo ->
-            println(repo.description)
-        }
+        .forEach { repo -> println(repo.description) }
 
     println("End")
 }

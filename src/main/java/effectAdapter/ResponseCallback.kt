65ffb9b228a5dc9661ad159c308539ff68fd3e0a
Leandro Borges Ferreira <lehen01@gmail.com>
2018-10-05 17:07:32 -0300

    fixing response callback response

--- src/main/java/effectAdapter/ResponseCallback.kt
+++ src/main/java/effectAdapter/ResponseCallback.kt
@@ -5,11 +5,21 @@ import arrow.core.left
 import arrow.core.right
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
 
-class ResponseCallback<R>(private val proc: (Either<Throwable, Response<R>>) -> Unit) : Callback<R> {
+class ResponseCallback<R>(private val proc: (Either<Throwable, R>) -> Unit) : Callback<R> {
     override fun onResponse(call: Call<R>, response: Response<R>) {
-        proc(response.right())
+        if (response.isSuccessful) {
+            val body = response.body()
+            if (body != null) {
+                proc(body.right())
+            } else {
+                proc(IllegalStateException("The request returned a null body").left())
+            }
+        } else {
+            proc(HttpException(response).left())
+        }
     }
 
     override fun onFailure(call: Call<R>, throwable: Throwable) {
